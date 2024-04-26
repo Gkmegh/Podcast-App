b6ffe80f16be71f478c497dc8dcf1c63fcae481d
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2024-04-26 18:59:07 +0530

    Update index.js

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -4,7 +4,7 @@ import './styles.css'
 function Loader() {
   return (
     <div className='loader-wrapper' >
-      <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
   )
 }
